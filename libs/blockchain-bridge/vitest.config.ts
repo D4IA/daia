5d26444e95eca84ba85dbe0681994e1df233cbd1
MrJustice26 <vladislav2074@gmail.com>
2025-11-09 22:19:18 +0100

    Get rid of @ aliases as we are using only internal aliases inside blockchain-bridge lib

--- libs/blockchain-bridge/vitest.config.ts
+++ libs/blockchain-bridge/vitest.config.ts
@@ -1,13 +1,6 @@
 import { defineConfig } from "vitest/config";
-import path from "path";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@src": path.resolve(__dirname, "src"),
-      "@types": path.resolve(__dirname, "types"),
-    },
-  },
   test: {
     globals: true,
     environment: "node",
