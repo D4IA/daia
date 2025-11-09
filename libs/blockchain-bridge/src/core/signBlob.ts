0b0fcafcb146fabbf017c7f107233e13acc77773
Władysław Kuczerenko <w.kuczerenko@crehler.com>
2025-11-09 15:56:04 +0100

    fix stuff

--- libs/blockchain-bridge/src/core/signBlob.ts
+++ libs/blockchain-bridge/src/core/signBlob.ts
@@ -2,14 +2,23 @@ import { PrivateKey } from "@bsv/sdk";
 
 /**
  * Signs a blob with a private key.
- * @param blob The blob to sign.
+ * @param blobOrArrayBuffer The blob or array buffer to sign.
  * @param privateKeyWif The private key in WIF format.
  * @returns The signature as a string in base64 format.
  */
-export const signBlob = async (blob: Blob, privateKeyWif: string) => {
-  const privKey = PrivateKey.fromWif(privateKeyWif);
+export const signBlob = async (
+  blobOrArrayBuffer: Blob | ArrayBuffer,
+  privateKeyWif: string
+) => {
+  let arrayBuffer: ArrayBuffer;
 
-  const arrayBuffer = await blob.arrayBuffer();
+  const privKey = PrivateKey.fromWif(privateKeyWif);
+  if (blobOrArrayBuffer instanceof Blob) {
+    arrayBuffer = await blobOrArrayBuffer.arrayBuffer();
+  } else {
+    arrayBuffer = blobOrArrayBuffer;
+  }
+
   const byteArray = Array.from(new Uint8Array(arrayBuffer));
 
   const signature = privKey.sign(byteArray);
