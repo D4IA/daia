2326d53a6a237b15b928670b0669531c303a0d21
MrJustice26 <vladislav2074@gmail.com>
2026-01-04 23:44:57 +0100

    Get rid of blockchain-bridge lib as we have consistent blockchain lib version for AI agents and UI communication

--- libs/blockchain-bridge/src/constants/apiEndpoints/base.const.ts
+++
@@ -1,5 +0,0 @@
-import { getBridgeConfig } from "#src/config";
-
-export const GET_BASE_URL = (network = getBridgeConfig().network): string => {
-  return `https://api.whatsonchain.com/v1/bsv/${network}`;
-};
--- libs/blockchain-bridge/src/constants/networkType.ts
+++
@@ -1,8 +0,0 @@
-import { Values } from "#types/values";
-
-export const NETWORK_TYPES = {
-    MAINNET: "main",
-    TESTNET: "test",
-} as const;
-
-export type NetworkType = Values<typeof NETWORK_TYPES>;
